17ebad6df02d72d85cc25b7c4ca3a3340cc01eb6
Garett Rogers <gootch2@gmail.com>
2025-03-22 19:30:46 -0700

    removing unused files

--- src/app/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useBlinkingCursor } from './useBlinkingCursor';
-export { useTerminalSize } from './useTerminalSize';
-export { useTypingAnimation } from './useTypingAnimation'; 
--- src/app/services/index.ts
+++
@@ -1,1 +0,0 @@
-export { terminalService } from './terminalService'; 
